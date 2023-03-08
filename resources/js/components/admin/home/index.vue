fe922daee23c20ad27f84944fd3b2569e0234a2d
stackarif <arifmahmudaj6@gmail.com>
2023-03-08 19:09:44 +0600

    portfolio website setup

---
+++ resources/js/components/admin/home/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Wecome to admin overview dashboard</h1>
+
+    <div>
+        <router-link to="/">Home page</router-link>
+    </div>
+</template>
---
+++ resources/js/components/notFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>
+        Not found page
+        
+    </h1>
+    <div>
+        <router-link to="/">Home page</router-link>
+    </div>
+</template>
